4dcef2643b023ee790307c808bd75e4229c2da5e
Gearoid <gocealla@gmail.com>
2020-03-11 21:33:51 +0100

    using handlebars

--- src/app.js
+++ src/app.js
@@ -7,35 +7,34 @@ const publicDirectoryPath = path.join(__dirname, '../publc')
 app.set ('view engine','hbs')
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req,res) => {
-    res.render('index')
+app.get('', (req,res) => {  // using render for hbs  as apposed to send for ht
+    res.render('index', { 
+        title: 'Weather App',
+        name: 'Carlos'
+    })
+})
+
+app.get('/help',(req,res)=> {
+    res.send([{
+        name:'harry',
+        age: 23
+    },
+    {
+        name:'Harry',
+        age: 44
+        
+    }])
 })
 
 app.get('/weather', (req, res) => {
     res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
+        forcast: 'Its raining',
+        location: 'Anywhere'
     })
 })
-
-const app= express()
-const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(path.join(__dirname,'../public')))
 
-
-app.get('/weatherapp',(req,res) =>{
-    res.send({
-        forcast: 50,
-        city: 'Dublin'
-
-    })
-})
-
-
-//app.com
-//app.com/help
-//app.com/about
 
 app.listen(3000,() => {
         console.log('server is up on port 3000')
