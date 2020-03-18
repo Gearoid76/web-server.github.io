86e000bd902ff53e2ef1803944d80138269f3c88
Gearoid <gocealla@gmail.com>
2020-03-18 16:22:11 +0100

    adding an error page

--- src/app.js
+++ src/app.js
@@ -7,10 +7,12 @@ const app = express()
 //before path for express config
 const publicDirectoryPath = path.join(__dirname, '../publc')
 const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
 
 //Setup handlebars enging and views location
 app.set ('view engine','hbs')
 app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
 
 //Setup static directiory to serve
 app.use(express.static(publicDirectoryPath))
@@ -18,22 +20,22 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req,res) => {  // using render for hbs  as apposed to send for ht
     res.render('index', { 
         title: 'Weather',
-        name: 'Carlos'
+        name:'Gearoid Hero2'
     })
 })
 
 app.get('/about', (req,res) => {
     res.render('about', {
     title: 'About',
-    name: 'Gigliii'
+    name:'Gearoid Hero2'
     })
 })
 
 app.get('/help',(req,res)=> {
     res.render('help', {
         title: 'Help',
-        name:'Hero2',
-        message:'This is the help message according to Horrible'
+        name:'Gearoid Hero2',
+        helpText:'This is the help message according to Horrible'
     })
 })
 
@@ -41,6 +43,23 @@ app.get('/weather', (req, res) => {
     res.send({
         forcast: 'Its raining',
         location: 'Anywhere'
+    })
+})
+
+app.get('/help/*',(req,res) => {
+    res.render('404',{
+        title:'404',
+        name: 'Gearoid Error Ok Ceallachain',
+        errorMessage:'Help page not found'
+    })
+})
+
+
+app.get('*',(req, res) => {
+    res.render('404',{
+        title: '404',
+        name: 'Gearoid Error Ok Ceallachain',
+        errorMessage: 'Page not found'
     })
 })
 
