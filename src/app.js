cb3b54965d15ddf74532a9d767f1eabee46856cc
Gearoid <gocealla@gmail.com>
2020-03-20 16:13:18 +0100

    updating files

--- src/app.js
+++ src/app.js
@@ -1,72 +1,67 @@
 const path = require('path')
 const express = require('express')
-const hbs = require ('hbs')
+const hbs = require('hbs')
 
 const app = express()
 
-//before path for express config
-const publicDirectoryPath = path.join(__dirname, '../publc')
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname,'../templates/partials')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-//Setup handlebars enging and views location
-app.set ('view engine','hbs')
-app.set('views',viewsPath)
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-//Setup static directiory to serve
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req,res) => {  // using render for hbs  as apposed to send for ht
-    res.render('index', { 
+app.get('', (req, res) => {
+    res.render('index', {
         title: 'Weather',
-        name:'Gearoid Hero2'
+        name: 'Gearoid MK OCeallachain'
     })
 })
 
-app.get('/about', (req,res) => {
+app.get('/about', (req, res) => {
     res.render('about', {
-    title: 'About',
-    name:'Gearoid Hero2'
+        title: 'About Me',
+        name: 'Gearoid Mk OCeallachain'
     })
 })
 
-app.get('/help',(req,res)=> {
+app.get('/help', (req, res) => {
     res.render('help', {
+        helpText: 'This is some helpful text.',
         title: 'Help',
-        name:'Gearoid Hero2',
-        helpText:'This is the help message according to Horrible'
+        name: 'Gearoid MK OCeallachain'
     })
 })
 
 app.get('/weather', (req, res) => {
     res.send({
-        forcast: 'Its raining',
-        location: 'Anywhere'
+        forecast: 'It is snowing',
+        location: 'Cork '
     })
 })
 
-app.get('/help/*',(req,res) => {
-    res.render('404',{
-        title:'404',
-        name: 'Gearoid Error Ok Ceallachain',
-        errorMessage:'Help page not found'
-    })
-})
-
-
-app.get('*',(req, res) => {
-    res.render('404',{
+app.get('/help/*', (req, res) => {
+    res.render('404', {
         title: '404',
-        name: 'Gearoid Error Ok Ceallachain',
-        errorMessage: 'Page not found'
+        name: 'Gearoid MK OCeallachain',
+        errorMessage: 'Help article not found.'
     })
 })
 
-app.use(express.static(path.join(__dirname,'../public')))
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Gearoid MK OCeallachain',
+        errorMessage: 'Page not found.'
+    })
+})
 
-
-app.listen(3000,() => {
-        console.log('server is up on port 3000')
-
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
 })
