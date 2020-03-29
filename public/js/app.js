6b3cb763915feebb88809e77010e17a00cfb30d7
Gearoid <gocealla@gmail.com>
2020-03-29 18:56:50 +0200

    added a heroku account to the program

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,26 @@
-console.log('Client side Javascript is loaded')
+console.log('Client side javascript file is loaded')
+
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-messageOne.textContent = console.log(data.error)
-messageTwo.textContent = console.log(data.location)
-messageTwo.textContent = console.log(data.forecast)
-
-   weatherForm.addEventListener('submit',(e) => {
-       e.preventDefault()
+weatherForm.addEventListener('submit', (e) =>  { e.preventDefault()
       
-       const location = search.value
+      const location = search.value
+       messageOne.textContent='Loading...'
+       messageTwo.textContent=''
 
-       fetch('http://localhost:3000/address=' + location).then((response) => {
+       fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) =>{
             if(data.error) {
-                console.log(data.error)
+                messageOne.textContent = data.error
 
             }else {
-                console.log(data.location)
-                console.log(data.forecast)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
+        })
     })
 })
-
-       console.log(location)
-   })
