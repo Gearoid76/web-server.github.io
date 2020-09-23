9cd960fae0d3bd5c6ed48a19ce36d8e383b209db
Gearoid <gocealla@gmail.com>
2020-09-23 11:02:22 +0300

    used Andrews code to see if this was the problem

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,24 @@
-console.log('Client side javascript file is loaded')
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-weatherForm.addEventListener('submit', (e) =>  { e.preventDefault()
-      
-      const location = search.value
-       messageOne.textContent='Loading...'
-       messageTwo.textContent=''
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-       fetch('/weather?address=' + location).then((response) => {
-    response.json().then((data) =>{
-            if(data.error) {
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
                 messageOne.textContent = data.error
-
-            }else {
+            } else {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
             }
         })
     })
-//fetch ('http://puzzle.mead.io/puzzle').then((response) => {
-//    response.json().then((dataa) => {
-//        console.log(data)
-//    })
-//})
 })
